a265f1b27031ae1c833fd331464f278c42f1586e
LammersInsects <marklammers6@gmail.com>
2022-08-18 22:37:26 +0200

    Remove full stop from db.process.actions for cases when no actions are found

--- db.process.actions.R
+++ db.process.actions.R
@@ -22,87 +22,88 @@ db.process.actions<-function(registry, #the previously saved registry
     stop()
   }
   if(!db.has.actions(registry = registry, quiet = T)){
-    cat(error('ERROR: No actions were found in the registry! Aborting...\n'))
-    stop()
-  }
-  
-  actions<-df[grep('db~',df[,3], fixed = T),]
-  
-  if(type=='remove' | type=='db~remove'){
-    todo<-'db~remove'
-  } else if(type=='translate' | type=='db~translate'){
-    todo<-'db~translate'
-  } else if(type=='all'){
-    todo<-c('db~remove','db~translate')
+    cat(warn('WARNING: No actions were found in the registry! Aborting...\n'))
+    cat(note('Unchanged registry is returned\n'))
   } else {
-    cat(error('Currently, only the actions <remove> and <translate> are supported\n'))
-    stop()
-  }
-  
-  #subset actions for those that are of the type requested to be processed
-  actions.todo<-actions[actions[,3] %in% todo,]
-  #process in antechronological order
-  actions.todo<-actions.todo[rev(rownames(actions.todo)),]
-  
-  #do the processing one by one
-  rm.count<-0
-  tr.count<-0
-  translated<-0
-  for(i in actions.todo$ID){
-    do<-actions.todo[actions.todo$ID==i,]
-    if(do[,3]=='db~remove'){
-      #remove target record
-      df<-df[df[,1]!=as.numeric(do[,4]),]
-      rm.count<-rm.count+1
-    } else if(do[,3]=='db~translate'){
-      #translate for the subject, field, value, and source column all values according to the action
-      for(column in 3:6){
-        translated<-translated+sum(df[,column]==do[,4],na.rm = T)
-        df[,column]<-gsub(do[,4],do[,5],df[,column],fixed = T)
-      }
-      tr.count<-tr.count+1
+    
+    actions<-df[grep('db~',df[,3], fixed = T),]
+    
+    if(type=='remove' | type=='db~remove'){
+      todo<-'db~remove'
+    } else if(type=='translate' | type=='db~translate'){
+      todo<-'db~translate'
+    } else if(type=='all'){
+      todo<-c('db~remove','db~translate')
     } else {
-      cat(error('ERROR: This line of code should should never be triggered! The following record causes a problem:\n'))
-      print(do)
+      cat(error('Currently, only the actions <remove> and <translate> are supported\n'))
+      stop()
     }
-    #remove the record of the action
-    df<-df[df[,1]!=i,]
-  }
-  
-  #report success
-  if(!quiet){
-    if(rm.count>0 & tr.count>0){
-      if((rm.count+tr.count)==nrow(actions.todo)){
-        cat(note(rm.count,'records were successfully removed and',translated,'cells had their contents translated by',
-                 tr.count,'translation actions\n'))
+    
+    #subset actions for those that are of the type requested to be processed
+    actions.todo<-actions[actions[,3] %in% todo,]
+    #process in antechronological order
+    actions.todo<-actions.todo[rev(rownames(actions.todo)),]
+    
+    #do the processing one by one
+    rm.count<-0
+    tr.count<-0
+    translated<-0
+    for(i in actions.todo$ID){
+      do<-actions.todo[actions.todo$ID==i,]
+      if(do[,3]=='db~remove'){
+        #remove target record
+        df<-df[df[,1]!=as.numeric(do[,4]),]
+        rm.count<-rm.count+1
+      } else if(do[,3]=='db~translate'){
+        #translate for the subject, field, value, and source column all values according to the action
+        for(column in 3:6){
+          translated<-translated+sum(df[,column]==do[,4],na.rm = T)
+          df[,column]<-gsub(do[,4],do[,5],df[,column],fixed = T)
+        }
+        tr.count<-tr.count+1
       } else {
-        cat(warn(nrow(actions.todo)-(rm.count+tr.count),'out of',nrow(actions.todo),
-                 'actions were not executed. Most likely these actions are removed by more recent actions\n'))
+        cat(error('ERROR: This line of code should should never be triggered! The following record causes a problem:\n'))
+        print(do)
       }
-      cat(note('Translated registry without the removed records is returned\n'))
-    } else if(rm.count>0){
-      if(rm.count==nrow(actions.todo)){
-        cat(note(rm.count,'records were successfully removed\n'))
+      #remove the record of the action
+      df<-df[df[,1]!=i,]
+    }
+    
+    #report success
+    if(!quiet){
+      if(rm.count>0 & tr.count>0){
+        if((rm.count+tr.count)==nrow(actions.todo)){
+          cat(note(rm.count,'records were successfully removed and',translated,'cells had their contents translated by',
+                   tr.count,'translation actions\n'))
+        } else {
+          cat(warn(nrow(actions.todo)-(rm.count+tr.count),'out of',nrow(actions.todo),
+                   'actions were not executed. Most likely these actions are removed by more recent actions\n'))
+        }
+        cat(note('Translated registry without the removed records is returned\n'))
+      } else if(rm.count>0){
+        if(rm.count==nrow(actions.todo)){
+          cat(note(rm.count,'records were successfully removed\n'))
+        } else {
+          cat(warn(nrow(actions.todo)-rm.count,'out of',nrow(actions.todo),
+                   'actions were not executed. Most likely these actions are removed by more recent actions\n'))
+        }
+        cat(note('Registry without the removed records is returned\n'))
+      } else if(tr.count>0){
+        if(tr.count==nrow(actions.todo)){
+          cat(note(translated,'cells had their contents translated by',tr.count,'translation actions\n'))
+        } else {
+          cat(warn(nrow(actions.todo)-tr.count,'out of',nrow(actions.todo),
+                   'actions were not executed. Most likely these actions are removed by more recent actions\n'))
+        }
+        cat(note('Translated registry is returned\n'))
       } else {
-        cat(warn(nrow(actions.todo)-rm.count,'out of',nrow(actions.todo),
-                 'actions were not executed. Most likely these actions are removed by more recent actions\n'))
+        if(nrow(actions.todo)>0){
+          cat(warn('No actions were executed, while actions were requested. Something went wrong?\n'))
+        } else {
+          cat(warn('No actions were executed because none of type',type,'is present in the registry\n'))
+        }
+        cat(note('Unchanged registry is returned\n'))
       }
-      cat(note('Registry without the removed records is returned\n'))
-    } else if(tr.count>0){
-      if(tr.count==nrow(actions.todo)){
-        cat(note(translated,'cells had their contents translated by',tr.count,'translation actions\n'))
-      } else {
-        cat(warn(nrow(actions.todo)-tr.count,'out of',nrow(actions.todo),
-                 'actions were not executed. Most likely these actions are removed by more recent actions\n'))
-      }
-      cat(note('Translated registry is returned\n'))
-    } else {
-      if(nrow(actions.todo)>0){
-        cat(warn('No actions were executed, while actions were requested. Something went wrong?\n'))
-      } else {
-        cat(warn('No actions were executed because none of type',type,'is present in the registry\n'))
-      }
-      cat(note('Unchanged registry is returned\n'))
     }
   }
   
